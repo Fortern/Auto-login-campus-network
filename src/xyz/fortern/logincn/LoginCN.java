d6aeaa49f977fc1e115031e46c2104c6cc455f82
Fortern <blueten.ki@gmail.com>
2021-04-11 16:32:42 +0800

    程序更新

--- src/xyz/fortern/logincn/LoginCN.java
+++ src/xyz/fortern/logincn/LoginCN.java
@@ -3,17 +3,22 @@ package xyz.fortern.logincn;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.*;
+import java.net.ConnectException;
+import java.net.URL;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
-import java.util.Scanner;
 
 public class LoginCN {
 	private static final Logger log = LogManager.getLogger(LoginCN.class);
+
 	//登录校园网
 	public static String login(String urlWithParam) {
 		StringBuilder result = new StringBuilder();
@@ -27,26 +32,24 @@ public class LoginCN {
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
 			String line;
 			while((line = in.readLine()) != null)
-				result.append("\n").append(line);
+				result.append(line).append("\n");
+			result.deleteCharAt(result.length()-1);//删除最后一个换行符
 			return result.toString();
 		} catch (ConnectException e) {
 			log.error("连接超时，请检查是否连接到校园网");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-			/*
-			//获取响应头不是必要的
-			Map<String, List<String>> map = conn.getHeaderFields();
-			for(String key: map.keySet()) {
-				System.out.println(key + "--->" + map.get(key));
-			}
-			*/
-			/*获取响应体
-			  登录成功时的响应体
-			  dr1004({"result":"1","msg":"\u8ba4\u8bc1\u6210\u529f"})
-			  已经登录的情况下再次登录的响应体
-			  dr1004({"result":"0","msg":"","ret_code":2})
-			*/
+		/*
+		  登录成功时的响应体:(以换行符为开头)
+		  dr1004({"result":"1","msg":"\u8ba4\u8bc1\u6210\u529f"})
+		  已经登录的情况下再次登录的响应体:
+		  dr1004({"result":"0","msg":"","ret_code":2})
+		  账号不存在时的响应体:
+		  dr1004({"result":"0","msg":"dXNlcmlkIGVycm9yMQ==","ret_code":1})
+		  密码错误:
+		  dr1004({"result":"0","msg":"dXNlcmlkIGVycm9yMg==","ret_code":1})
+		 */
 		return null;
 	}
 	//获取局域网IP地址
@@ -111,25 +114,24 @@ public class LoginCN {
 	}
 	public static void main(String[] args) {
 		log.info("--- 程序启动 ---");
-		Scanner input = null;
-		String path;
+		String account = null;
+		String password = null;
+		String isp = null;
 		try {
-			path = LoginCN.class.getClassLoader().getResource("user.txt").getPath();
-			path = URLDecoder.decode(path, "utf-8");
-			input = new Scanner(new File(path));
+			//jar中的文件，只能以流的方式读取
+			BufferedReader reader = new BufferedReader(new InputStreamReader(LoginCN.class.getClassLoader().getResourceAsStream("user.txt")));
+			account = reader.readLine();
+			password = reader.readLine();
+			switch(reader.readLine()) {
+				case "1": isp = "@telecom";break;
+				case "2": isp = "@unicom";break;
+			}
 		} catch (Exception e) {
 			log.error("无法读取用户数据,请检查后手动运行程序");
 			e.printStackTrace();
 			System.exit(0);
 		}
-		String account = input.nextLine();
-		String password = input.nextLine();
-		String isp = "";
-		switch(input.nextLine()) {
-			case "1": isp = "@telecom";break;
-			case "2": isp = "@unicom";break;
-		}
-		String surl = "http://172.28.0.22:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=" +
+		String url = "http://172.28.0.22:801/eportal/?c=Portal&a=login&callback=dr1004&login_method=1&user_account=" +
 				account + isp + "&user_password=" + password + "&wlan_user_ip=";
 		while(true){
 			log.info("尝试获取ip地址");
@@ -145,8 +147,8 @@ public class LoginCN {
 				continue;
 			}
 			log.info("尝试发送登录请求");
-			String s = login(surl + ip);
-			if(s==null){
+			String result = login(url + ip);
+			if(result == null){
 				log.info("30秒后尝试重新登录");
 				try {
 					Thread.sleep(30000);
@@ -155,20 +157,22 @@ public class LoginCN {
 				}
 				continue;
 			}
-			log.info("响应体：" + s);
-			switch(s.charAt(18)){
-				case '1':
-				case '2':
-					while(isConnect()){
-						try {
-							Thread.sleep(180000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}continue; //跳转到while的开头
-				case '3':
-					log.error("登录失败，请检查登录信息是否正确，然后重启程序");
-					System.exit(0);
+			log.info("响应体：" + result);
+			//如果返回登录成功或已经登录的消息，则检查一次网络连接
+			if(result.charAt(41) == '2' || result.charAt(18) == '1') {
+				while(isConnect()){
+					log.info("已连接到Internet");
+					try {
+						Thread.sleep(180000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				log.warn("网络已断开，将重新登录");
+				//continue;
+			}else{
+				log.error("登录失败，请检查登录信息是否正确，然后重启程序");
+				System.exit(0);
 			}
 		}
 	}
